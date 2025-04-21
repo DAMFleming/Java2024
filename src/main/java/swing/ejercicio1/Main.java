602e11b7f41f995a4cb4dcef5b4399869d0e734a
Julio Pedro López Bayón <juliolb@educastur.org>
2025-04-21 20:36:57 +0200

    Actualización

--- src/main/java/swing/ejercicio1/Main.java
+++ src/main/java/swing/ejercicio1/Main.java
@@ -1,30 +1,45 @@
 package swing.ejercicio1;
 
 import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
-public class Main {
+public class Main extends JFrame {
 	
-	private final JFrame frame = new JFrame("Multicontador");
-	
-    Main() {
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.getContentPane().setLayout(new GridLayout(0, 1));
+	private static final long serialVersionUID = 1L;
+
+	Main() {
+    	setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    	getContentPane().setLayout(new GridLayout(0, 1));
     	ContadorGlobal contadorGlobal = new ContadorGlobal(20);
     	for (int i=0; i<3; i++)
-    		frame.add(new ContadorParcial(20, contadorGlobal));
-        frame.add(contadorGlobal);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-    }
-
-    public void show() {
-        frame.setVisible(true);
+    		add(new ContadorParcial(20, contadorGlobal));
+        add(contadorGlobal);
+        pack();
+        setLocationRelativeTo(null);
+        addWindowListener(new WindowAdapter() {
+        	@Override
+        	public void windowClosing(WindowEvent e) {
+        		int respuesta = JOptionPane.showConfirmDialog(
+        				Main.this,
+        				"¿Cerrar la aplicaicón?",
+        				"Multicontador",
+        				JOptionPane.YES_NO_OPTION);
+        		if (respuesta == JOptionPane.YES_OPTION) {
+        			dispose();
+        			System.exit(0);
+        		}
+        	}
+        });
     }
 
     public static void main(final String[] args) {
-        SwingUtilities.invokeLater(() -> new Main().show());
+        SwingUtilities.invokeLater(() -> new Main().setVisible(true));
     }
+
+	
 }
