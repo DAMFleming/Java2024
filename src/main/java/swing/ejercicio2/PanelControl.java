8f644c134e5373526972d3ed7439366509b82d2f
Andrea Milena Vega Mora <mgd02452@educastur.es>
2025-04-23 17:47:13 +0200

    actualizacion

--- src/main/java/swing/ejercicio2/PanelControl.java
+++ src/main/java/swing/ejercicio2/PanelControl.java
@@ -1,22 +1,71 @@
 package swing.ejercicio2;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * Creamos los botones para controlar las fases de la luna, recibe referencia a
+ * PanelLuna ya que ahi es donde estan todas las fotos
+ * 
+ * En el listener de la clase PanelLuna hemos hecho dos metodos para que cambie
+ * automaticamente de foto sin tener que usar botones
+ * 
+ * 
+ * - Tarea: sustituir los botones por un JSlider (barra de desplazamiento),
+ * tienen que cambiar las fases de la luna y el Slider desplazandose solo al
+ * hacer clic en la luna hay que dehabilitar los botones cuando la animacion
+ * esté en marcha y volver a habilitarlo cuando la animacion se pare
+ * 
+ */
 
 public class PanelControl extends JPanel {
 
 	private PanelLuna panelLuna;
-	
+	private JSlider slider;
+
+	JButton izq;
+	JButton der;
+
 	public PanelControl(PanelLuna panelLuna) {
 		this.panelLuna = panelLuna;
-		JButton ant = new JButton(new ImageIcon(getClass().getResource("/luna/anterior.png")));
-		ant.addActionListener(panelLuna::anterior);
-		JButton sgte = new JButton(new ImageIcon(getClass().getResource("/luna/siguiente.png")));
-		sgte.addActionListener(panelLuna::siguiente);
-		add(ant);
-		add(sgte);
-		
+
+		slider = new JSlider(0, 7, 0); // mínimo, máximo, valor inicial
+		slider.setMajorTickSpacing(1);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+
+		// Notificar a PanelLuna cuando el usuario mueve el slider
+		slider.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				if (!slider.getValueIsAdjusting()) {
+					panelLuna.setIndice(slider.getValue());
+				}
+			}
+		});
+
+		panelLuna.setSlider(slider);
+
+		// imagen de los botones
+		izq = new JButton(new ImageIcon(getClass().getResource("/luna/anterior.png")));
+		izq.addActionListener(panelLuna::anterior);
+		der = new JButton(new ImageIcon(getClass().getResource("/luna/siguiente.png")));
+		der.addActionListener(panelLuna::siguiente);
+
+		add(izq);
+		add(der);
+		add(slider);
+
 	}
-	
+
+	//este metodo establece cuando los botones esten activos
+	public void setControlesActivos(boolean activo) {
+		izq.setEnabled(activo);
+		der.setEnabled(activo);
+
+	}
 }
--- src/main/java/swing/ejercicio2/PanelLuna.java
+++ src/main/java/swing/ejercicio2/PanelLuna.java
@@ -1,21 +1,48 @@
 package swing.ejercicio2;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JLabel;
-import javax.swing.Timer;
+import javax.swing.*;
+
+/**
+ * Aqui se crean los recursos para mostrar las imagenes (jLabel)
+ * 
+ * 	La clase ImageIcon nos permite cargar imagenes y JLabel las necesita La clase
+ * BufferedImage se necesita un framework E/S de imagenes para cargarlas
+ * 
+ * Cuando se haga clic sobre la luna, se deben pasar automaticamente todas las
+ * fases cada medio segundo y cuando llega a la ultima vuelve al inicio
+ * 
+ * Cuando se le da clic otra vez se para el bucle
+ * 
+ * 
+ * 	La clase Timer nos va a servir para hacer animaciones simples y chapuceras
+ * 
+ * 
+ */
 
 public class PanelLuna extends JLabel {
 
 	private static final long serialVersionUID = 1L;
-	private static final Luna [] LUNAS = new Luna[8];
+	private static final Luna[] LUNAS = new Luna[8];
+
+	private int indiceActual = 0;
+	private Timer timer;
+	//private Timer timer = new Timer(500, this::siguiente); otra opcion con los botones implementados
 	
+	private boolean animando = false;
 	private int fase;
-	private Timer timer = new Timer(500, this::siguiente);
-	
+	private JSlider slider;
+	private PanelControl panelControl;
+
+	// Inicialización de las fases
 	static {
 		LUNAS[0] = new Luna("Luna Nueva");
 		LUNAS[1] = new Luna("Luna Creciente");
@@ -26,37 +53,72 @@ public class PanelLuna extends JLabel {
 		LUNAS[6] = new Luna("Cuarto Menguante");
 		LUNAS[7] = new Luna("Luna Menguante");
 	}
-	
-	public PanelLuna(int fase) {
-		super(LUNAS[fase].getFase(), LUNAS[fase].getIcon(), JLabel.CENTER);
+
+	public PanelLuna(int fase) { // con el parametro me permite crear el panel con la fase de la luna que yo quiera, no que empiece siempre en [0]
+		super(LUNAS[fase].getFase(), LUNAS[0].getIcon(), JLabel.CENTER);
 		this.fase = fase;
+		
 		setVerticalTextPosition(JLabel.BOTTOM);
 		setHorizontalTextPosition(JLabel.CENTER);
 		setForeground(Color.WHITE);
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if (timer.isRunning())
-					timer.stop();
-				else
-					timer.start();
-			}
-		});
+
+		// Crear el Timer con intervalo de medio segundo = 500 milisegundos
+		timer = new Timer(500, e -> mostrarSiguienteFase()); //el constructor pide un intervalo de tiempo y un Listener
+
+
+		/*
+		 * Listener para el slider, botones y clic en la imagen, cuando esté en animacion el slider no se podra usar
+		 * No se pueden poner dos addMouseListener ya que se sobreescriben y el Timer se activa y desactiva todo el rato
+		 */
+		 addMouseListener(new MouseAdapter() {
+		        @Override
+		        public void mouseClicked(MouseEvent e) {
+		            if (animando) {
+		                timer.stop(); // metodos de la clase Timer, si no corre se para y sino continua hasta que se pare
+		                if (slider != null) slider.setEnabled(true);
+		                if (panelControl != null) panelControl.setControlesActivos(true); //no hay animacion en curso
+		            } else {
+		                timer.start();
+		                if (slider != null) slider.setEnabled(false);
+		                if (panelControl != null) panelControl.setControlesActivos(false); //para los botones
+		            }
+		            animando = !animando;
+		        }
+		    });
+	}
+
+	private void mostrarSiguienteFase() {
+		// el indice hace q incremente 1 foto   
+		indiceActual = (indiceActual + 1) % LUNAS.length; //asegura que el valor se mantenga dentro del rango válido del array (0 a 7, 8 fases)
+		setText(LUNAS[indiceActual].getFase());
+		setIcon(LUNAS[indiceActual].getIcon());
+		 if (slider != null) {
+	            slider.setValue(indiceActual); //segun la posicion de la fto, el indice del slider coincide con la posicion
+	        }
+	}
+	
+	public void setPanelControl(PanelControl panelControl) {
+	    this.panelControl = panelControl;
 	}
 
 	
+	/**
+	 * Funcionalidad de los botones
+	 */
+
 	public void anterior(ActionEvent e) {
-		if (--fase < 0)
-			fase = LUNAS.length - 1;
+		if(--fase < 0) // para que cuando llegue al final, vuelva al inicio
+			fase = LUNAS.length -1;
 		setFase();
 	}
-	
+
 	public void siguiente(ActionEvent e) {
-		if (++fase == LUNAS.length)
+		if(++fase == LUNAS.length)  // para que al darle al boton de siguiente, se vayan aumentando las lunas, las fotos
 			fase = 0;
 		setFase();
 	}
 	
+	// permite mostrar por la GUI las fotos con su respectivo texto
 	private void setFase() {
 		setIcon(LUNAS[fase].getIcon());
 		setText(LUNAS[fase].getFase());
@@ -67,4 +129,25 @@ public class PanelLuna extends JLabel {
 		setFase();
 	}
 	
+	
+	/**
+	 * Slider
+	 */
+
+    private void actualizarVista() {
+        setText(LUNAS[indiceActual].getFase());
+        setIcon(LUNAS[indiceActual].getIcon());
+    }
+
+    // Llamado desde el slider, cambia la fase manualmente desde el slider
+    public void setIndice(int i) {
+        indiceActual = i;
+        actualizarVista();
+    }
+
+    // Llamado desde PanelDeControl, para que pueda pasar la referencia del slider
+    public void setSlider(JSlider slider) {
+        this.slider = slider;
+    }
 }
+
