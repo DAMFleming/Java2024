2e1668e64191d824e6d4eaa3bb5aa978615cd42c
Julio Pedro López Bayón <jplbayon@gmail.com>
2025-05-02 19:08:35 +0200

    Actualización

--- src/main/java/java2d/ejemplo/Lienzo.java
+++ src/main/java/java2d/ejemplo/Lienzo.java
@@ -10,6 +10,8 @@ import javax.swing.JComponent;
 
 public class Lienzo extends JComponent {
 
+	private static final long serialVersionUID = 1L;
+
 	public Lienzo(int ancho, int alto) {
 		setPreferredSize(new Dimension(ancho , alto));
 	}
