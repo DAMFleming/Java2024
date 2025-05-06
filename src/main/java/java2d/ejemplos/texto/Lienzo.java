ff707379e7e286b8b8aa0d0411b7dbd7eafbf96a
Julio Pedro López Bayón <juliolb@educastur.org>
2025-05-06 16:51:36 +0200

    Actualización

---
+++ src/main/java/java2d/ejemplos/texto/Lienzo.java
@@ -0,0 +1,49 @@
+package java2d.ejemplos.texto;
+
+import java.awt.BasicStroke;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.FontMetrics;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.io.IOException;
+
+
+public class Lienzo extends Canvas {
+
+	private static final long serialVersionUID = 1L;
+	private static final BasicStroke STROKE = new BasicStroke(10.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER);
+	private static final Paint PAINT1 = new GradientPaint(50, 50,Color.RED, 350, 350, Color.WHITE);
+	private static final Paint PAINT2 = new GradientPaint(500, 50, Color.RED, 800, 350, Color.WHITE);
+	private static final Font FONT;
+	
+	static {
+		try {
+			FONT = Font.createFont(Font.PLAIN, Lienzo.class.getResourceAsStream("/fuentes/Heraldic Shadows.otf"));
+		} catch (IOException | FontFormatException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	public Lienzo(int ancho, int alto) {
+		setFont(FONT.deriveFont(70f));
+		setPreferredSize(new Dimension(ancho , alto));
+		setBackground(Color.WHITE);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		FontMetrics fm = g.getFontMetrics();
+		
+		
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawString("Texto de ejemplo", 10, 10);
+	}
+}
